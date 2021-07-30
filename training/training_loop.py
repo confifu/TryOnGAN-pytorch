6a9c824f2dcf248a44e2328b68745288c9cf572b
Saurabh Kumar <saurabhk660@gmail.com>
2021-07-30 06:29:47 +0530

    fix problems with segmap in dataset

--- training/training_loop.py
+++ training/training_loop.py
@@ -230,6 +230,7 @@ def training_loop(
         print('Exporting sample images...')
         grid_size, images, labels, pose, parsemaps = setup_snapshot_image_grid(training_set=training_set)
         save_image_grid(images, os.path.join(run_dir, 'reals.png'), drange=[0,255], grid_size=grid_size)
+        save_image_grid(parsemaps, os.path.join(run_dir, 'parsemaps.png'), drange=[0,7], grid_size=grid_size)
         grid_z = torch.randn([labels.shape[0], G.z_dim], device=device).split(batch_gpu)
         grid_c = torch.from_numpy(labels).to(device).split(batch_gpu)
         images = torch.cat([G_ema(z=z, c=c, noise_mode='const').cpu() for z, c in zip(grid_z, grid_c)]).numpy()
